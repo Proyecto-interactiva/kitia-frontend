c54f13192acaf2f60b65b2a04a73e63fe445aa66
Katherine Páez Ramos <slakat@gmail.com>
2026-01-14 09:35:23 -0300

    Updates

--- _app/immutable/chunks/BcUcWO48.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1p9xz8w?.base??"",a=globalThis.__sveltekit_1p9xz8w?.assets??s;export{a,s as b};
---
+++ _app/immutable/chunks/BnHEcNbp.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1yhwr01?.base??"",a=globalThis.__sveltekit_1yhwr01?.assets??s;export{a,s as b};
